2f60b68da039a14ff8d84cfb3b28b7cacfd77e04
Sora23DE <142969818+Sora23DE@users.noreply.github.com>
2023-08-23 09:05:19 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+
+package Src;
+
+
+
+class Main {
+
+    double currentNumber = 0;
+
+    public double add(double toAdd)
+    {
+        
+
+        return currentNumber;
+    }
+
+    
+
+
+
+    public static void main(String[] args) 
+    {
+    
+    }
+
+}
